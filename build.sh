c6548b8bf0743d8f86a9dd950cc87a90375758e5
Danielzapirtan <antoniudanielzapirtan@gmail.com>
2024-10-10 04:18:31 +0300

    Install binary outside repo.

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #! /usr/bin/bash
 
-gcc -o baeagn baeagn.c -lm -O4 -march=native -w
-
+gcc -o baeagn baeagn.c -lm -O4 -march=native -w || exit 2
+install -m 0755 -s baeagn /usr/local/bin
+rm -rf baeagn
