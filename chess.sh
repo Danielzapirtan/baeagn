2f9e0c878acaac3d6f8da884172a3633ad9f4c6f
Danielzapirtan <antoniudanielzapirtan@gmail.com>
2025-04-27 02:36:34 +0300

    chess

---
+++ chess.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+username=antoniudanielzapirtan
+url=https://api.chess.com/pub/player/$username/games/to-move
+
+gh run list --status in_progress | wc  -l
+curl $url 2>/dev/null | wc -l
+
