4d2bc28abb2eaa1b3f3c65f1d9447d116b347443
Danielzapirtan <137394552+Danielzapirtan@users.noreply.github.com>
2025-04-26 14:03:49 +0300

    Update bench.sh

--- bench.sh
+++ bench.sh
@@ -2,9 +2,10 @@
 
 PGN="$1"
 DEPTH=$2
+GAMESYMBOL=$3
 echo "$PGN" $DEPTH
 ulimit -t 21000
 rm -r start.pgn
 cp input/game.pgn start.pgn
-baeagn $DEPTH | tee start.d$DEPTH.anl
+baeagn $DEPTH | tee $GAMESYMBOL.d$DEPTH.anl
 
