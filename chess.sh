4a2edc4b70038efd49bdd34aee6c0ca7f0cf96e2
Danielzapirtan <137394552+Danielzapirtan@users.noreply.github.com>
2025-04-27 13:58:24 +0300

    Update chess.sh

--- chess.sh
+++ chess.sh
@@ -10,3 +10,8 @@ cat $tempfile | tail -n 6
 cat $tempfile | wc  -l
 curl $url 2>/dev/null | sed -e "s/daily/\n/g" | wc -l
 
+# Replace with your Chess.com API URL
+API_URL="https://api.chess.com/pub/player/antoniudanielzapirtan/games"
+
+# Fetch JSON data and extract PGN values
+curl -s "$API_URL" | jq -r '.games[].pgn'
