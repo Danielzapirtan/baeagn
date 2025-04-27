b46f6471c8411e6fc7b8de6b73dc2ea81a59543c
Danielzapirtan <antoniudanielzapirtan@gmail.com>
2025-04-27 03:18:29 +0300

    Bugfix

--- chess.sh
+++ chess.sh
@@ -4,5 +4,5 @@ username=antoniudanielzapirtan
 url=https://api.chess.com/pub/player/$username/games/to-move
 
 gh run list --status in_progress | wc  -l
-curl $url 2>/dev/null | grep daily | wc -l
+curl $url 2>/dev/null | sed -e "s/daily/\n/g" | wc -l
 
