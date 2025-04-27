aba48cda830b042f7e07592c70f86aa32b71cd44
Danielzapirtan <antoniudanielzapirtan@gmail.com>
2025-04-27 09:23:57 +0300

    m

--- chess.sh
+++ chess.sh
@@ -4,7 +4,7 @@ username=antoniudanielzapirtan
 url=https://api.chess.com/pub/player/$username/games/to-move
 
 date +%H:%M
-gh run list --status in_progress | cut -f 8
+gh run list --status in_progress | tail -n 5 | cut -f 8
 gh run list --status in_progress | wc  -l
 curl $url 2>/dev/null | sed -e "s/daily/\n/g" | wc -l
 
