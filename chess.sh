d49fd5697362ec8dbdaa054715f89cd82de92895
Danielzapirtan <antoniudanielzapirtan@gmail.com>
2025-04-27 08:29:29 +0000

    chess

--- chess.sh
+++ chess.sh
@@ -1,10 +1,12 @@
 #! /bin/bash
 
+tempfile=/tmp/gh.tmp
 username=antoniudanielzapirtan
 url=https://api.chess.com/pub/player/$username/games/to-move
 
 date +%H:%M
-gh run list --status in_progress | tail -n 5 | cut -f 8
-gh run list --status in_progress | wc  -l
+gh run list | grep in_progress >$tempfile
+cat $tempfile | tail -n 6
+cat $tempfile | wc  -l
 curl $url 2>/dev/null | sed -e "s/daily/\n/g" | wc -l
 
