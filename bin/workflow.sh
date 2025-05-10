53db515dade008764add2f5ac469b30894a5e3eb
Danielzapirtan <antoniudanielzapirtan@gmail.com>
2025-05-10 23:07:29 +0300

    m

--- bin/workflow.sh
+++ bin/workflow.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
 GAMESYMBOL=$1
+TIMELIMIT=$2
 
-gh workflow run play_move.yml -f game_symbol=$GAMESYMBOL --ref main
+gh workflow run play_move.yml -f game_symbol=$GAMESYMBOL -f timelimit=$TIMELIMIT --ref main
 
