960fa0587465af0070d6e1d63b9bea59f54ad8c2
Danielzapirtan <137394552+Danielzapirtan@users.noreply.github.com>
2025-05-14 15:33:25 +0300

    Update bench.sh

--- bin/bench.sh
+++ bin/bench.sh
@@ -5,6 +5,5 @@ GAMESYMBOL=$2
 echo $GAMESYMBOL $DEPTH
 rm -rf start.pgn
 cp pgn/$GAMESYMBOL.pgn start.pgn
-./baeagn "$(cat start.pgn)" | tee $GAMESYMBOL.d$DEPTH.anl &
-cpulimit -e baeagn -l 50
-
+(./baeagn "$(cat start.pgn)" | tee $GAMESYMBOL.d$DEPTH.anl) &
+cpulimit -e baeagn -l 20
