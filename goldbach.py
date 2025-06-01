78b596570acab764723cbe6f5389f744bd2d6616
Danielzapirtan <137394552+Danielzapirtan@users.noreply.github.com>
2025-06-01 15:59:28 +0300

    Update goldbach.py

--- goldbach.py
+++ goldbach.py
@@ -8,11 +8,28 @@ def pr(p):
         k += 2
     return True
 
-def gbn(n):
+def prtab(l):
+    p = 3
+    tabpr = [2]
+    while p < l:
+        if pr(p):
+            tabpr.append(p)
+        p += 2
+    return tabpr
+
+def longpr(p, tab):
+    for k in tab:
+        if k > p // k:
+            return True
+        if p % k == 0:
+            return False
+    return True 
+
+def gbn(n, tab):
     p = 3
     while p <= n - p:
         if pr(p):
-            if pr(n - p):
+            if longpr(n - p, tab):
                return True
         p += 2
     return False
