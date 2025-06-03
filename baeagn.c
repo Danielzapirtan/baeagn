e57d9a0f3c59ad2af3494c71bb46d8392cba9bc0
Danielzapirtan <137394552+Danielzapirtan@users.noreply.github.com>
2025-06-03 20:59:38 +0300

    Update baeagn.c

--- baeagn.c
+++ baeagn.c
@@ -152,7 +152,7 @@ const VALUE _BETA_DFL     = (+20000);
 const VALUE _MAXVALUE     = (20000);
 const VALUE _PAWNUNIT     = (100);
 const VALUE _THRESHOLD    = (15000);
-const VALUE _VALUES[6]    = { 0, 100, 315, 325, 500, 980, };
+const VALUE _VALUES[6]    = { 0, 100, 445, 325, 615, 980, };
 
 ELAPSED elapsed;
 LEVEL gdepth;
