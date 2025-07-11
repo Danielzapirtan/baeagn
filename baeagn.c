edeb3201f83a36f0e81a3d5311e59c08c757ebc9
Danielzapirtan <137394552+Danielzapirtan@users.noreply.github.com>
2025-07-11 18:11:00 +0300

    Update baeagn.c

--- baeagn.c
+++ baeagn.c
@@ -376,7 +376,7 @@ VALUE eval(BOARD board, LEVEL level)
     if ((nodes % _SKIPFRAMES) == 0) {
         update(&elapsed);
         double delapsed = dclock(&elapsed);
-        if (delapsed >= 21000.0)
+        if (delapsed >= 600.0)
             exit(0);
     }
     for (y = 0; y < 8; y++)
