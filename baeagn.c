bcdd5558aa3d178c75ea9eac42f0fc0980f7fdb5
Danielzapirtan <antoniudanielzapirtan@gmail.com>
2025-09-18 04:02:51 +0000

    m

--- baeagn.c
+++ baeagn.c
@@ -433,7 +433,7 @@ VALUE eval(BOARD board, LEVEL level)
     VALUE pvalue = 0;
     VALUE value;
     nodes++;
-    if (nodes > 7L<<33)
+    if (nodes > 1L<<33)
 	    exit(0);
     if ((nodes % _SKIPFRAMES) == 0) {
         update(&elapsed);
