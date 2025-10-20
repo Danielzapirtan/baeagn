8be2860286504792983a26a5d0fec02b9fef89a6
Danielzapirtan <137394552+Danielzapirtan@users.noreply.github.com>
2025-10-20 20:55:48 +0300

    Update baeagn.c

--- baeagn.c
+++ baeagn.c
@@ -22,8 +22,8 @@
 #define _BRDFILE "start.brd"
 #define _FENFILE "start.fen"
 
-#define _ALPHA (-50) // Adjusted as needed
-#define _BETA (50)
+#define _ALPHA (-20000) // Adjusted as needed
+#define _BETA (20000)
 #define _OVERDEPTH (1)
 #define _S_DEPTH (4)
 #define _SORT
