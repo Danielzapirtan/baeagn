1d8d71e97fe147cc1f31414f30ff48f83ddb3272
Danielzapirtan <137394552+Danielzapirtan@users.noreply.github.com>
2025-10-29 21:08:02 +0200

    Fix output redirection for FEN and PGN data

--- dlpgn.sh
+++ dlpgn.sh
@@ -21,14 +21,20 @@ n=0
 while true; do
 	myurlc=$(jq -c .games[$n].url /tmp/games.txt)
 	if [ x"$myurl" = x"$myurlc" ]; then
+		fen=$(jq -r .games[$n].fen /tmp/games.txt)
+		if [ x"$fen" != x"null" ] && [ x"$fen" != x"" ]; then
+			echo "[FEN \"$fen\"]" >pgn/bench.pgn
+		else
+			>pgn/bench.pgn
+		fi
 		jq -c .games[$n].pgn /tmp/games.txt | \
 			grep -o "[[:digit:]]\+\.\+ [[:alnum:]+-=]\+ " \
-			>pgn/bench.pgn
+			>>pgn/bench.pgn
 		        echo "*" >>pgn/bench.pgn
 		jq -c .games[$n].white /tmp/games.txt
 		jq -c .games[$n].black /tmp/games.txt
+		jq -c .games[$n].rules /tmp/games.txt
 		exit 0
 	fi
 	n=$(($n + 1))
 done
-
