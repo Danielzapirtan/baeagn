0505d62b627318d844f0f661cb5109b694578dbf
Danielzapirtan <antoniudanielzapirtan@gmail.com>
2025-11-04 23:07:23 +0000

    Try

--- batch.sh
+++ batch.sh
@@ -2,8 +2,6 @@
 
 COUNT=$1
 myltg=$2
-TZ=EET
-export TZ
 
 for n in $(seq 1 $COUNT); do
 	mygn=$(($n - 1))
@@ -13,10 +11,10 @@ for n in $(seq 1 $COUNT); do
 	cat /tmp/bench.yml \
 		| sed -e "s/myltg/$myltg/g" \
 		>.github/workflows/bench.yml
-	date=$(date +%Y%m%d-%H%M)
+	date=$(date +%Y%m%d-%H%M%S)
 	echo $date
 	sh wf 1 >~/$date.txt 2>&1 &
-	sleep 60
+	sleep 5
 done
 echo "All workflows triggered"
 
