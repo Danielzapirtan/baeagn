41b842a26b24128664f8595c8167327b2bbd3e43
Danielzapirtan <antoniudanielzapirtan@gmail.com>
2025-11-05 21:32:03 +0200

    yaccg

--- batch.sh
+++ batch.sh
@@ -1,22 +1,19 @@
 #! /bin/sh
 
 COUNT=$1
-SESSION_TIME=86400
-myltg=$(($SESSION_TIME / $COUNT))
+SESSION_TIME=$2
+myltg=$SESSION_TIME
 
-for n in $(seq 1 $COUNT); do
-	mygn=$(($n - 1))
-	cat bench.yml \
-		| sed -e "s/mygn/$mygn/g" \
-		>$HOME/bench.yml
-	cat $HOME/bench.yml \
-		| sed -e "s/myltg/$myltg/g" \
-		>.github/workflows/bench.yml
-	date=$(date +%Y%m%d-%H%M%S)
-	echo $date
-	delay=$(($myltg + 60))
-	sh wf 1 $delay >~/$date.txt 2>&1 &
-	sleep 5
-done
+cat bench.yml \
+	| sed -e "s/mygn/$mygn/g" \
+	>$HOME/bench.yml
+cat $HOME/bench.yml \
+	| sed -e "s/myltg/$myltg/g" \
+	| sed -e "s/count/$COUNT/g" \
+	>.github/workflows/bench.yml
+date=$(date +%Y%m%d-%H%M%S)
+echo $date
+delay=$(($myltg + 60))
+sh wf 1 >~/$date.txt 2>&1 &
 echo "All workflows triggered"
 
