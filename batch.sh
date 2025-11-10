6236c4d0809e5ac4bfcb4f0a4cf076dd944d8712
Danielzapirtan <antoniudanielzapirtan@gmail.com>
2025-11-10 15:34:28 +0000

    Ecart

--- batch.sh
+++ batch.sh
@@ -5,25 +5,28 @@ NPROCESSORS=4
 url1="https://api.chess.com/pub/player/$user/games/to-move"
 curl -s $url1 >/tmp/games1.txt
 COUNT=$(jq '.games | length' /tmp/games1.txt)
-if [ $COUNT -gt $NPROCESSORS ]; then
-	COUNT=$NPROCESSORS
-fi
 SESSION_TIME=21000
-if true; then
-if [ $COUNT -lt $NPROCESSORS ]; then
-	NPROCESSORS=$COUNT
+REMAINING=$COUNT
+ECART=0
+
+while true; do
+if [ $REMAINING -lt $NPROCESSORS ]; then
+	exit
 fi
-fi
+REMAINING=$(($REMAINING - $NPROCESSORS))
+COUNT=$NPROCESSORS
 myltg=$(($SESSION_TIME * $NPROCESSORS / $COUNT))
 
 cat bench.yml \
 	| sed -e "s/mygn/$mygn/g" \
 	| sed -e "s/myltg/$myltg/g" \
-	| sed -e "s/count/$COUNT/g" \
+	| sed -e "s/ecart/$ECART/g" \
 	>.github/workflows/bench.yml
 date=$(date +%Y%m%d-%H%M%S)
 echo $date
 sh wf >~/$date.txt 2>&1 &
 echo "$COUNT diagrams"
+ECART=$(($ECART + 4))
+done
 echo "All workflows triggered"
 
