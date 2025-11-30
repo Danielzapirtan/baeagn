959a6d3e808d66199488b8ba5b3ad26195a73878
Danielzapirtan <antoniudanielzapirtan@gmail.com>
2025-11-30 17:04:26 +0200

    yaccg

--- ore.sh
+++ ore.sh
@@ -3,10 +3,10 @@
 PGNFILE=base/sea.pgn
 
 movelist=$(cat $PGNFILE \
-  | grep -o "^6\.Be3 [^ ]\+ " \
-  | sort -u | awk '{print $2}')
+  | grep -o "^6\.Be3 e5 [^ ]\+ " \
+  | sort -u | awk '{print $3}')
 
 for move in $movelist; do
   echo -n "$move "
-  cat $PGNFILE | grep "^6\.Be3 $move "|wc -l
+  cat $PGNFILE | grep "^6\.Be3 e5 $move "|wc -l
 done|sort -nk 2
