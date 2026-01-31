1af029d85a1be4dcabb6f1b87544a69194ec1d6c
Danielzapirtan <antoniudanielzapirtan@gmail.com>
2026-01-31 13:02:21 +0000

    yaccg

--- batch.sh
+++ batch.sh
@@ -8,7 +8,7 @@ url1="https://api.chess.com/pub/player/$user/games/to-move"
 curl -s $url1 >$HOME/games1.txt
 COUNT=$(jq '.games | length' $HOME/games1.txt)
 COUNTF=$COUNT
-SESSION_TIME=7200
+SESSION_TIME=1000
 REMAINING=$COUNT
 ECART=0
 
