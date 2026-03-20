c1e6bc35e9e664c9e2a3951232236e14c176e433
Danielzapirtan <antoniudanielzapirtan@gmail.com>
2026-03-20 23:57:44 +0200

    Modify d6 bonus ammount

--- iccf.c
+++ iccf.c
@@ -431,9 +431,9 @@ VALUE eval(BOARD board, LEVEL level)
     }
 #if 1
     if (board[5][3] == -1)
-        pvalue += 211;
+        pvalue += 33;
     if (board[2][3] == +1)
-	pvalue -= 211;    
+	pvalue -= 33;    
 #endif
     if (kings) {
     if (kings > 0)
