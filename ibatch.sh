62092d0a0135e064991739d8f9abdede5303a835
Danielzapirtan <antoniudanielzapirtan@gmail.com>
2026-03-26 13:50:19 +0000

    date

--- ibatch.sh
+++ ibatch.sh
@@ -4,6 +4,6 @@ git add .
 git commit -miccf
 git push
 gh workflow run iccf
-(sleep 18000; /data/data/com.termux/files/usr/bin/termux-notification --title "Baeagn/ICCF" --content "Analyzed ok") &
+(sleep 18000; /data/data/com.termux/files/usr/bin/termux-notification --title "Baeagn/ICCF" --content "Analyzed ok @ $(date +%H:%M)") &
 echo "Workflow triggered ok"
 
