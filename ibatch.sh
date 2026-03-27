fff1381efef557a01f4758ff8f71be6a40db7639
Danielzapirtan <antoniudanielzapirtan@gmail.com>
2026-03-27 13:55:41 +0200

    iccf

--- ibatch.sh
+++ ibatch.sh
@@ -1,6 +1,18 @@
 #! /bin/bash
 
 export TZ=Europe/Bucharest
+export PLAYER="$1"
+export GLIST=pgn/gamelist.txt
+
+date
+: > $GLIST
+for c in w b; do
+	GAME=$PLAYER$c
+	echo $GAME
+	cat - > pgn/$GAME.pgn
+	echo $GAME > $GLIST
+done
+
 git add .
 git commit -miccf
 git push
